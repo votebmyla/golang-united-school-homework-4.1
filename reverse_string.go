f32416c6ec62f4093b881d914ecaafc4e9346119
votebmyla <votebmyla@gmail.com>
2022-05-13 22:27:13 +0500

    fixed rune

--- reverse_string.go
+++ reverse_string.go
@@ -1,9 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	byteSlice := []byte(input)
-	for i := len(byteSlice); i > 0; i-- {
-		byteSlice = append(byteSlice, byteSlice[i-1])
+	runeSlice := []rune(input)
+	for i := len(runeSlice); i > 0; i-- {
+		runeSlice = append(runeSlice, runeSlice[i-1])
 	}
-	return string(byteSlice[len(byteSlice)/2:])
+	return string(runeSlice[len(runeSlice)/2:])
 }
