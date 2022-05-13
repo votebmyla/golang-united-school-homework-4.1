7db032fd3d80353047bec3c12c0bab20477fb665
votebmyla <votebmyla@gmail.com>
2022-05-13 22:23:47 +0500

    first task solution

--- reverse_string.go
+++ reverse_string.go
@@ -1,6 +1,9 @@
 package reverse_string
 
 func ReverseString(input string) (output string) {
-	// solution goes here
-	return output
+	byteSlice := []byte(input)
+	for i := len(byteSlice); i > 0; i-- {
+		byteSlice = append(byteSlice, byteSlice[i-1])
+	}
+	return string(byteSlice[len(byteSlice)/2:])
 }
